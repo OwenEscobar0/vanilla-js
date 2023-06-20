efae1ae3de4caa84fb4d70fc626be68570881a9c
Owen Esconar <owenescobar200080@gmail.com>
2023-06-20 15:49:47 -0600

    agregando cambios

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,21 @@
-// Inserte el código aquí
+//let arreglo = [1,2,3,4];
 
-
+//function clonarArreglo(arreglo){
+//let lista = [...arreglo];
+//console.log(lista);
+//return lista;
+//}
 
 // NO MODIFICAR
+
 export { clonarArreglo, clonarObjeto, sumar, saludar };
+
+let objeto = ["🐮", "-🐺", "🦒",];
+let objeto2 = ["🐔"];
+
+function clonarObjeto() {
+  let lista = objeto + objeto2;
+  console.log(lista);
+  return lista;
+}
+clonarObjeto();
