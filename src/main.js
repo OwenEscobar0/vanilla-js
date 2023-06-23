0cd6fd23c0111b2b6c436ae03b4ec14b732baa02
Owen Esconar <owenescobar200080@gmail.com>
2023-06-23 15:59:31 -0600

    reloj digital

---
+++ src/main.js
@@ -0,0 +1,17 @@
+function horaactual() {
+    let date = new Date();
+    let hora = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let horaActual = hora + ":" + minutes + ":" + seconds;
+  
+    let eti =document.getElementById("time")
+  
+  
+  
+  
+    eti.innerHTML = horaActual;
+  }
+  
+  horaactual()
+  setInterval(horaactual, 1000);
